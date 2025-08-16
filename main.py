c9d7b396541866d3631d8c2bfb52fd7e4420fcaa
Naval Kishor <navalkishor8525@gmail.com>
2025-08-16 12:14:06 +0530

    created main file

--- main.py
+++ main.py
@@ -18,14 +18,6 @@ def computer_choice():
     choice_list = ['Rock', 'Paper', 'Scissors']
     computer = random.choice(choice_list)
     return computer
-    # random_number = random.randint(1,3)
-    # if (random_number==1):
-    #     computer = "Rock"
-    # elif (random_number==2):
-    #     computer = "Paper"
-    # else:
-    #     computer = "Scissors"
-    # return computer
 
 def take_input_from_user():
     # Take input from user until user enter the valid input
